a78679d2c8f30f3304cfa83e1ed5f03647fcb967
joaomiguel13 <a2020122715@isec.pt>
2024-05-17 15:30:01 +0100

    Fix na board e -thread.c da bolsa(Não estava a ser utilizado)

--- board/threads.c
+++ board/threads.c
@@ -26,14 +26,18 @@ BOOL WINAPI receiveInfoFromBolsa(LPVOID p) {
 			if (i + 1 <= sharedMemory->sharedData->numEmpresas) {
 				_tprintf(_T("Empresa %d\n"), i);
 				_tprintf(_T("Nome: %s\n"), sharedMemory->sharedData->empresas[i].nome);
+
 				_tprintf(_T("Ações disponíveis: %d\n"), sharedMemory->sharedData->empresas[i].acoesDisponiveis);
+
 				_tprintf(_T("Preço da ação: %.2f\n"), sharedMemory->sharedData->empresas[i].precoAcao);
+				
+				
 				_tprintf(_T("----------------------------\n"));
 			}
 		}
 		_tprintf(_T("Ultima transação\n"));
 		_tprintf(_T("Empresa: %s\n"), sharedMemory->sharedData->lastTransacao.empresa.nome);
-		_tprintf(_T("Numero de ações: %d\n"), sharedMemory->sharedData->lastTransacao.numAcoes);
+		_tprintf(_T("Número de ações: %d\n"), sharedMemory->sharedData->lastTransacao.numAcoes);
 		_tprintf(_T("Preço das ações: %.2f\n"), sharedMemory->sharedData->lastTransacao.precoAcoes);
 		_tprintf(_T("=============================\n"));
 		ResetEvent(sharedMemory->hEventUpdateBoard);
